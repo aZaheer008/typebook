6a6229b31380fa5eaaaedfeaf78702da439c0ad7
unknown <kmitinstitute008@gmail.com>
2021-08-22 15:26:45 +0500

    code execution with safety requirements so, wrong code should give error , it would not execute as code

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import { useState , useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import { useRef } from 'react';
 
 const App = () => {
 
+  const ref= useRef<any>();
+  const iframe= useRef<any>();
   const [input, setInput] = useState('');
-  const [code ,setCode] = useState('');
 
   const onClick = async () => {
     console.log(input);
@@ -16,6 +18,8 @@ const App = () => {
     //   loader:'jsx',
     //   target:'es2015'
     // });
+
+    iframe.current.srcdoc = html;
 
     const result = await esbuild.build({
       entryPoints : ['index.js'],
@@ -29,10 +33,30 @@ const App = () => {
         global:'window'
       }
     });
-    console.log("----result----",result)
- 
-    setCode(result.outputFiles[0].text);
+    // console.log("----result----",result)
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text,'*');
   };
+
+  const html = `
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message',(event) => {
+          try {
+            eval(event.data);
+          } catch (err){
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+            console.error(err);
+          }
+        },false);
+      </script>
+    </body>
+  </html>
+  `;
 
   useEffect(() => {
     esbuild.initialize({
@@ -48,7 +72,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
+      <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
     </div>
   )
 };
